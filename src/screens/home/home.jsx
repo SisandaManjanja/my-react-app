c0cb23f9cda7b88fc3c3b3ef56939dca8a12490a
sisandaemm@gmail.com <Sisanda Manjanja>
2024-02-07 00:14:47 +0200

    Homepage

---
+++ src/screens/home/home.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./style.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="div">
+        <div className="text-wrapper">Sisanda Manjanja</div>
+        <div className="text-wrapper-2">Fullstack software developer</div>
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="text-wrapper-3">About</div>
+        </div>
+        <img
+          className="logo"
+          alt="Logo"
+          src="https://cdn.animaapp.com/projects/65c1ea393bc5360edd664453/releases/65c1ea72b77185cd73cafacf/img/logo.png"
+        />
+        <div className="overlap-group">
+          <div className="bxs-home-svg">
+            <div className="text-wrapper-4">H</div>
+          </div>
+          <div className="rectangle-2" />
+          <img
+            className="vector"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/65c1ea393bc5360edd664453/releases/65c1ea72b77185cd73cafacf/img/vector.svg"
+          />
+          <img
+            className="img"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/65c1ea393bc5360edd664453/releases/65c1ea72b77185cd73cafacf/img/vector-1.svg"
+          />
+          <img
+            className="vector-2"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/65c1ea393bc5360edd664453/releases/65c1ea72b77185cd73cafacf/img/vector-2.svg"
+          />
+          <img
+            className="vector-3"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/65c1ea393bc5360edd664453/releases/65c1ea72b77185cd73cafacf/img/vector-3.svg"
+          />
+          <img
+            className="vector-4"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/65c1ea393bc5360edd664453/releases/65c1ea72b77185cd73cafacf/img/vector-4.svg"
+          />
+          <div className="text-wrapper-5">P</div>
+          <div className="text-wrapper-6">S</div>
+          <div className="text-wrapper-7">C</div>
+          <div className="text-wrapper-8">?</div>
+          <div className="rectangle-3" />
+        </div>
+      </div>
+    </div>
+  );
+};
