ca5398b08763be9fa1b53bb93ccb06052fffcb8a
sisandaemm@gmail.com <Sisanda Manjanja>
2024-02-07 23:41:36 +0200

    fixing the navigation bar.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './screens/home'; // Correcting to 'Home' assuming it's the correct component name
 import reportWebVitals from './reportWebVitals';
+import { About } from './screens/home/about';
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <About/>
   </React.StrictMode>,
   document.getElementById('root')
 );
